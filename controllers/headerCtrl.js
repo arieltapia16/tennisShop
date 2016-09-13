44cf5ddd0fc28ebc43aa5f999cc3504cc61658c8
Ariel Tapia <arielt@bandofcoders.com>
2016-09-13 16:55:31 -0300

    ng-repeat for header popup

--- controllers/headerCtrl.js
+++ controllers/headerCtrl.js
@@ -1,8 +1,70 @@
 angular.module("app")
 .controller("headerCtrl", function($scope, $state){
-  $scope.option = function(opt){
-    // console.log(opt);
-    $scope.articles_option = opt
-  }
+    $scope.option = function(opt){
+        // console.log(opt);
+        $scope.articles_option = opt
+    }
+
+    $scope.popups = [
+
+        {
+            title : 'Tennis',
+            option: 't',
+            items :[
+                {
+                    name:'Raquetas',
+                    url:'/'
+                },
+                {
+                    name:'Indumentaria',
+                    url:'/'
+                },
+                {
+                    name:'Grips',
+                    url:'/'
+                },
+                {
+                    name:'Pelotas',
+                    url:'/'
+                },
+                {
+                    name:'Calzado',
+                    url:'/'
+                },
+                {
+                    name:'Accesorios',
+                    url:'/'
+                },
+
+            ]
+        },
+        {
+            title : 'Padel',
+            option: 'p',
+            items :[
+                {
+                    name:'Raquetas',
+                    url:'/'
+                },
+                {
+                    name:'Indumentaria',
+                    url:'/'
+                },
+                {
+                    name:'Pelotas',
+                    url:'/'
+                },
+
+                {
+                    name:'Accesorios',
+                    url:'/'
+                },
+
+            ]
+        }
+
+
+
+    ]
 
 })

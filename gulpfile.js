1506f591cbcff06cabcf2f5f09b0e070186718fe
arieltapia16 <arieltapia16@gmail.com>
2016-09-14 20:32:21 -0300

    gilp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,30 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
     sass = require('gulp-sass'),
-    serve = require('gulp-serve');
+    livereload = require('gulp-livereload'),
+    serve = require('gulp-serve'),
+    browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function() {
-    gulp.src('sass/personal.scss')
+    gulp.src('./sass/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./style/'));
 });
 
+gulp.task('serve', serve('')); //serve in localhost:3000
+// gulp.task('livereload')
+
+// gulp.task('serve', function() {
+  // livereload.listen();
+
+  // browserSync({
+  //   server: {
+  //     baseDir: '',
+  //   },
+  //  port: 8080
+  // });
+// })
   //Watch task
 gulp.task('default',function() {
     gulp.watch('sass/personal.scss',['sass']);
